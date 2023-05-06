c776bb39bd78eaa5245d2dc7a3b8fc1f40256985
Olga <olakostina31@gmail.com>
2023-05-06 12:25:44 +0300

    Fixed src

--- src/test/java/WebPageTest.java
+++ src/test/java/WebPageTest.java
@@ -2,7 +2,10 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -12,9 +15,15 @@ public class WebPageTest {
     public void shouldGetMessage() {
         open("http://localhost:9999");
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "A");
-        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder='Дата встречи']").setValue("25.04.2023");
+        $("[data-test-id=date] [value]").click();
+        LocalDate dateDefault = LocalDate.now().plusDays(3);
+        LocalDate dateOfMeeting = LocalDate.now().plusDays(30);
+        String stringToSearch = dateOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
+        if (dateOfMeeting.getMonthValue()>dateDefault.getMonthValue()|dateOfMeeting.getYear()>dateDefault.getYear()){
+            $(".calendar__arrow_direction_right[data-step='1']").click();
+        }
+        $$("td.calendar__day").find(exactText(dayToSearch)).click();
         $x("//input[@name='name']").setValue("Николай Васильев");
         $x("//input[@name='phone']").setValue("+78888888888");
         $x("//label[@data-test-id='agreement']").click();
