d8b02f2ba4ac79ad7880bba7bd4ea4343b57dd37
Olga <olakostina31@gmail.com>
2023-05-07 11:43:34 +0300

    Added changes according to the task

--- src/test/java/WebPageTest.java
+++ src/test/java/WebPageTest.java
@@ -11,19 +11,17 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class WebPageTest {
 
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     public void shouldGetMessage() {
         open("http://localhost:9999");
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] [value]").click();
-        LocalDate dateDefault = LocalDate.now().plusDays(3);
-        LocalDate dateOfMeeting = LocalDate.now().plusDays(30);
-        String stringToSearch = dateOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
-        if (dateOfMeeting.getMonthValue()>dateDefault.getMonthValue()|dateOfMeeting.getYear()>dateDefault.getYear()){
-            $(".calendar__arrow_direction_right[data-step='1']").click();
-        }
-        $$("td.calendar__day").find(exactText(dayToSearch)).click();
+        String currentDate = generateDate(5, "dd.MM.yyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(currentDate);
         $x("//input[@name='name']").setValue("Николай Васильев");
         $x("//input[@name='phone']").setValue("+78888888888");
         $x("//label[@data-test-id='agreement']").click();
